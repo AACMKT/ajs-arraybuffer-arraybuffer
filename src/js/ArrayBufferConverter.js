e558b609e06180c7e8eef95f0c9597d70a68a37c
Maxim E <maermolenko@mail.ru>
2024-02-24 14:29:45 +0300

    Minor changes commit

--- src/js/ArrayBufferConverter.js
+++ src/js/ArrayBufferConverter.js
@@ -1,16 +1,17 @@
 export default class ArrayBufferConverter {
-   constructor() {
+  constructor() {
     this.bufferView = new Uint16Array();
-   }
-    load(buffer) {
-        this.bufferView = new Uint16Array(buffer);
-    };
+  }
 
-    stringify() {
-        let str = '';
-        for (let i = 0; i < this.bufferView.length; i += 1) {
-            str = str.concat(String.fromCharCode(this.bufferView[i]));
-        };
-        return str;
+  load(buffer) {
+    this.bufferView = new Uint16Array(buffer);
+  }
+
+  stringify() {
+    let str = '';
+    for (let i = 0; i < this.bufferView.length; i += 1) {
+      str = str.concat(String.fromCharCode(this.bufferView[i]));
     }
-};
+    return str;
+  }
+}
--- src/js/__tests__/arrBuffConv_tests.js
+++ src/js/__tests__/arrBuffConv_tests.js
@@ -2,7 +2,7 @@ import getBuffer from '../getBuffer';
 import ArrayBufferConverter from '../ArrayBufferConverter';
 
 test('ArrayBufferConverter methods test', () => {
-    const toStr = new ArrayBufferConverter;
-    toStr.load(getBuffer());
-    expect(toStr.stringify()).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}')
-})
+  const toStr = new ArrayBufferConverter();
+  toStr.load(getBuffer());
+  expect(toStr.stringify()).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
+});
